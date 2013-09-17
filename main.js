8a972dfc9282b24ca5a1e6cf9d2e04588f015181
mdix <dix@mediaman.de>
2013-09-17 10:53:45 +0200

    Ok, we now have health and monsters and you might die on your adventures

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ var character = function(x, y) {
 	this.y = y;
 	this.symbol = "\033[34m♖\033[0m";
 	this.gold = 0;
+	this.health = 100;
+	this.monsterKilled = 0;
 }
 
 var randomCharacter = new character(4,4);
@@ -24,34 +26,47 @@ function main() {
 	render(randomCharacter);
 	moveCharacter(randomCharacter);
     spawnGold();
+	spawnMonster();
 }
 
 function render(randomCharacter) {
 	process.stdout.write('\u001B[2J\u001B[0;0f');
 	
-	for (var x = 0; x < level.length; x++) {
-		for (var y = 0; y < level[x].length; y++) {
-			if (level[x][y] < 0) {
-				if (randomCharacter.x === x && randomCharacter.y === y) {
-					process.stdout.write(randomCharacter.symbol);
-				} else {
-					process.stdout.write('❖');
-				}
-			} else {
-				if (randomCharacter.x === x && randomCharacter.y === y) {
-					process.stdout.write(randomCharacter.symbol);
-				} else {
-					if (level[x][y] === 2) {
-						process.stdout.write("\033[33m☻\033[0m");	
+	if (randomCharacter.health === 0) {
+	    process.stdout.write("Aw snap, you died! Your stats:");
+		process.stdout.write("\n");
+		process.stdout.write("\n");
+	} else {
+		for (var x = 0; x < level.length; x++) {
+			for (var y = 0; y < level[x].length; y++) {
+				if (level[x][y] < 0) {
+					if (randomCharacter.x === x && randomCharacter.y === y) {
+						process.stdout.write(randomCharacter.symbol);
 					} else {
-						process.stdout.write('☐');	
+						process.stdout.write('❖');
+					}
+				} else {
+					if (randomCharacter.x === x && randomCharacter.y === y) {
+						process.stdout.write(randomCharacter.symbol);
+					} else {
+						if (level[x][y] === 2) {
+							process.stdout.write("\033[33m☻\033[0m");	
+						} else if (level[x][y] === 3) {
+							process.stdout.write("\033[31m☠\033[0m");
+						} else {
+							process.stdout.write('☐');	
+						}
 					}
 				}
 			}
+			process.stdout.write("\n");
 		}
-		process.stdout.write("\n");
 	}
     process.stdout.write("gold: " + randomCharacter.gold);
+	process.stdout.write("\n");
+    process.stdout.write("health: " + randomCharacter.health);
+	process.stdout.write("\n");
+    process.stdout.write("monster killed: " + randomCharacter.monsterKilled);
 }
 
 function moveCharacter(randomCharacter) {
@@ -69,6 +84,7 @@ function moveCharacter(randomCharacter) {
 					randomCharacter.x = nextX;
 					randomCharacter.y = nextY;
 					pickGold(nextX, nextY);
+					fightMonster(nextX, nextY);
 					isWalkable = true;	
 				}
 			}
@@ -94,10 +110,36 @@ function spawnGold() {
 	}
 }
 
+function spawnMonster() {
+	if (time % 20 === 0) {
+		var maxY = level[0].length - 1;
+		var maxX = level.length - 1;
+		var isWalkable = false;
+		
+		while (isWalkable !== true) {
+			posX = Math.floor((Math.random()*maxX)+1);
+			posY = Math.floor((Math.random()*maxY)+1);
+		
+			if (level[nextX][nextY] > 0) {
+				level[nextX][nextY] = 3;
+				isWalkable = true;
+			}
+		}
+	}
+}
+
 function pickGold(characterPositionX, characterPositionY) {
 	if (level[characterPositionX][characterPositionY] === 2) {
 		randomCharacter.gold++;
 		level[characterPositionX][characterPositionY] = 1;
+	}
+}
+
+function fightMonster(characterPositionX, characterPositionY) {
+	if (level[characterPositionX][characterPositionY] === 3) {
+		randomCharacter.health -= Math.floor((Math.random()*10)+1);;
+		level[characterPositionX][characterPositionY] = 1;
+		randomCharacter.monsterKilled++;
 	}
 }
 
